cac3861dc7bf8fc8972f990161397bb3c71606aa
Riabov, Evgenii <eriabov@ru.estee.com>
2020-09-03 14:51:37 +0300

    Added Equals and Hashcode for edge type.

--- src/main/java/name/dan1els/simplegraph/Edge.java
+++ src/main/java/name/dan1els/simplegraph/Edge.java
@@ -1,13 +1,13 @@
 package name.dan1els.simplegraph;
 
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
+@EqualsAndHashCode
 @ToString
 public class Edge<ID, T> {
     
     private final Vertex<ID, T> to;
-    //TODO: TBD
-    private final double weight = 1;
     
     public Edge(Vertex<ID, T> to) {
         this.to = to;
